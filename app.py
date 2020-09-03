9ff4067d4d2e953d84bd6d300f1399b9006e99ce
Enio Fernandes <EN10@users.noreply.github.com>
2020-09-03 22:13:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,3 @@ def home():
 		return render_template('index.html', users=users)
    	else:
    		return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
