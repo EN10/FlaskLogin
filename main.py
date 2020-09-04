94bccf19a3b3ae839675ec8e2c210be028b1443b
Enio Fernandes <EN10@users.noreply.github.com>
2020-09-04 11:31:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import sqlite3
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Hello, World!1'
+
+@app.route('/create')
+def create_table():
+    sql = """   CREATE TABLE Product(
+				ProductID integer,
+				Name text,
+				Price real,
+				Primary Key(ProductID))
+          """
+    with sqlite3.connect("coffee_shop.db") as db:
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+    return 'CREATE TABLE'
