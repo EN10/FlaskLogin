61bf262d14207493a25004cccbf66d4d6530f548
Enio Fernandes <EN10@users.noreply.github.com>
2020-09-04 10:57:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from flask import Flask
-from flask import render_template
-from flask import request
-import models as dbHandler
-
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def home():
-	if request.method=='GET':
-		return render_template('index.html')
-	elif request.method=='POST':
-		username = request.form['username']
-   		password = request.form['password']
-   		dbHandler.insertUser(username, password)
-   		users = dbHandler.retrieveUsers()
+    return 'Hello, World!'
