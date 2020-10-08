4454213dbb185ccac4de01de748812074a9f3a3e
eniompw <71254710+eniompw@users.noreply.github.com>
2020-10-08 15:56:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for
 import sqlite3
 import os
 from markupsafe import escape
