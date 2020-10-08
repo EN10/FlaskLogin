54a016b5a8af09828ed3665a1023720baef07be8
eniompw <71254710+eniompw@users.noreply.github.com>
2020-10-08 15:52:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,64 +20,20 @@ def login():
 def signup():
 	return render_template('signup.html')
 
-def create():
-	with sqlite3.connect('login.db') as db:
-		cursor = db.cursor()
-		cursor.execute(	"""	CREATE TABLE IF NOT EXISTS Users(
-						Username text,
-						Password text,
-						Primary Key(Username))
-				""")
-		db.commit()
-	print('CREATE')
 create()
 
 @app.route('/insert')
-def insert():
-	with sqlite3.connect('login.db') as db:
-		cursor = db.cursor()
-		cursor.execute(	"""	INSERT INTO Users (Username, Password)
-						VALUES ("Bob", "123")
-				""")
-		db.commit()
-	return 'INSERT'
+insert():
 
 @app.route('/select')
-def select():
-	try:
-		with sqlite3.connect('login.db') as db:
-			cursor = db.cursor()
-			cursor.execute("SELECT * FROM Users")
-			result = cursor.fetchall()
-			if len(result) == 0:
-				return 'no records'
-			else:
-				return ','.join(map(str, result))
-	except Exception as e:
-		return str(e)
+select():
 
 @app.route('/add', methods=['POST'])
-def add():
-	with sqlite3.connect('login.db') as db:
-		cursor = db.cursor()
-		cursor.execute(	"INSERT INTO Users (Username, Password) VALUES (?,?)",
-			       		(request.form['uname'],request.form['psw']))
-		db.commit()
-	return request.form['uname'] + ' added'
+add():
 
 @app.route('/verify', methods=['POST'])
-def verify():
-	with sqlite3.connect('login.db') as db:
-		cursor = db.cursor()
-		cursor.execute(	"SELECT * FROM Users WHERE Username=? AND Password=?",
-			       (request.form['uname'],request.form['psw']))
-		result = cursor.fetchall()
-		if len(result) == 0:
-			return 'username / password not recognised'
-		else:
-			session.permanent = True
-			session['username'] = request.form['uname']
-			return 'welcome ' + request.form['uname']
+verify():
+
 @app.route('/un')
 def un():
 	if 'username' in session:
