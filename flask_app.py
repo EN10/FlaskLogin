e9515742944e7370ed08a23788aa4239cdb63f0f
eniompw <71254710+eniompw@users.noreply.github.com>
2020-11-17 16:22:20 +0000

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template
+import sqlite3
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/create')
+def create():
+	with sqlite3.connect('login.db') as db:
+		cursor = db.cursor()
+		cursor.execute(	"""	CREATE TABLE Users(
+						Username text,
+						Password text,
+						Primary Key(Username))
+				""")
+		db.commit()
+	return 'CREATE'
+
+@app.route('/insert')
+def insert():
+	with sqlite3.connect('login.db') as db:
+		cursor = db.cursor()
+		cursor.execute(	"""	INSERT INTO Users (Username, Password)
+						VALUES ("Bob", "123")
+				""")
+		db.commit()
+	return 'INSERT'
+
+@app.route('/select')
+def select():
+	try:
+		with sqlite3.connect('login.db') as db:
+			cursor = db.cursor()
+			cursor.execute("SELECT * FROM Users")
+			result = cursor.fetchall()
+			if len(result) == 0:
+				return 'no records'
+			else:
+				return ','.join(map(str, result))
+	except Exception as e:
+		return str(e)
