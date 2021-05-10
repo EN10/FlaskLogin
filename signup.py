9d68d7531cb3676d22dd22006b69b7a883fcba28
eniompw <71254710+eniompw@users.noreply.github.com>
2021-05-10 15:20:06 +0100

    Update signup.py

--- signup.py
+++ signup.py
@@ -7,8 +7,11 @@ def home():
     if request.method == 'POST':
         con = sqlite3.connect('login.db')
         cur = con.cursor()
-		cur.execute("INSERT INTO Users (Username, Password) VALUES (?,?)",
-			       		(request.form['un'],request.form['pw']))
+        try:
+            cur.execute("INSERT INTO Users (Username, Password) VALUES (?,?)",
+                (request.form['un'],request.form['pw']))
+        except Exception as e:
+            return str(e)
         con.commit()
         return request.form['un'] + ' added'
     else:
