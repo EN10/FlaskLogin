05f0bbe7842779ad7a64d4181bcf13d734ce0832
eniompw <71254710+eniompw@users.noreply.github.com>
2021-05-10 15:11:33 +0100

    Update signup.py

--- signup.py
+++ signup.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        name = request.form['un']
-        return 'hello ' + name
+        con = sqlite3.connect('login.db')
+        cur = con.cursor()
+		cur.execute("INSERT INTO Users (Username, Password) VALUES (?,?)",
+			       		(request.form['un'],request.form['pw']))
+        con.commit()
+        return request.form['un'] + ' added'
     else:
         return render_template('simple_form.html')
