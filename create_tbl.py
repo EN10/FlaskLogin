4be724f3e36c87f9567196a77a57513e3320bd16
eniompw <71254710+eniompw@users.noreply.github.com>
2021-10-07 16:26:57 +0100

    Update create_tbl.py

--- create_tbl.py
+++ create_tbl.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    con = sqlite3.connect("ben.db")
+    con = sqlite3.connect("Login.db")
     cur = con.cursor()
     try:
         cur.execute("""
